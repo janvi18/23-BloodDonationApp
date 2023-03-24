498f37d04b337f37fedee5c09a39b0a51dc3353e
janvi18 <sjanvi9333@gmail.com>
2023-03-24 23:32:08 +0530

    dashboard

---
+++ app/src/main/java/com/blooddonationapp/DonorDashboardActivity.java
@@ -0,0 +1,14 @@
+package com.blooddonationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonorDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_dashboard);
+    }
+}
--- app/src/main/java/com/blooddonationapp/SplashActivity.java
+++ app/src/main/java/com/blooddonationapp/SplashActivity.java
@@ -1,14 +1,44 @@
 package com.blooddonationapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Animation topAnimation, bottomAnimation;
+    private ImageView logo;
+    private TextView title, slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.title);
+        slogan = findViewById(R.id.slogan);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.to_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        logo.setAnimation(topAnimation);
+        title.setAnimation(bottomAnimation);
+        slogan.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 4000);
     }
 }
